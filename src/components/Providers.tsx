8039e7b70dceda7118ae6e75b6ed48efa1db2148
Igor Kuzmenko <igor@kuzzmi.com>
2024-12-13 23:26:10 +0200

    upd

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 import { PlayerProvider } from "./Player/PlayerContext";
+
+function ForceThemeProvider({ children }: { children: React.ReactNode }) {
+  const { theme, resolvedTheme } = useTheme();
+
+  console.log(theme, resolvedTheme);
+
+  return children;
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
-        <PlayerProvider>{children}</PlayerProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+        <ForceThemeProvider>
+          <PlayerProvider>{children}</PlayerProvider>
+        </ForceThemeProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
