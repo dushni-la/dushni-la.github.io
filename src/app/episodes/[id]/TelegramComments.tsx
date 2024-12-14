3e0f5a6002d223a95eff8f32e49e9abaffb2503b
Igor Kuzmenko <igor@kuzzmi.com>
2024-12-14 12:53:36 +0200

    fixes tg comments

--- src/app/episodes/[id]/TelegramComments.tsx
+++ src/app/episodes/[id]/TelegramComments.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card, CardBody, semanticColors } from "@nextui-org/react";
+import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const TelegramComments: React.FC<{ id: number | string }> = ({ id }) => {
@@ -15,19 +16,16 @@ const TelegramComments: React.FC<{ id: number | string }> = ({ id }) => {
 
   return (
     <Card>
-      <CardBody className="bg-background">
+      <CardBody className="bg-background overflow-y-visible">
         <iframe
           id="telegram-discussion"
-          src={`https://t.me/dushnila_podcast/${id}?embed=1&discussion=1&comments_limit=5&dark=${isDark ? "1" : "0"}&color=${semanticColors.dark.default[500]}`}
+          src={`https://t.me/dushnila_podcast/${id}?embed=1&discussion=1&dark=${isDark ? "1" : "0"}&color=${semanticColors.dark.default[500]}`}
           width="100%"
           height="500"
           frameBorder="0"
-          scrolling="no"
           style={{
-            overflow: "hidden",
             colorScheme: isDark ? "dark" : "light",
             border: "none",
-            height: 500,
           }}
         />
       </CardBody>
