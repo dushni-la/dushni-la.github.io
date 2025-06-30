bd3a772f4125d851c3153f7c93bf71c51dca0036
Igor Kuzmenko <igor@kuzzmi.com>
2025-07-01 00:32:05 +0300

    upds sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,6 +16,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: `${BASE_URL}/course`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.95,
+    },
+    {
       url: `${BASE_URL}/episodes`,
       lastModified: new Date(),
       changeFrequency: "weekly",
